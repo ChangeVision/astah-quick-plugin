11ac2d053c927d2e754f3ceaa1d1006decb4acde
Hiroki Kondo <kompiro@gmail.com>
2013-03-01 16:04:09 +0900

    set command name when command candidate is only one and commit command

--- src/main/java/com/change_vision/astah/quick/internal/ui/candidatesfield/CommitCommandAction.java
+++ src/main/java/com/change_vision/astah/quick/internal/ui/candidatesfield/CommitCommandAction.java
@@ -13,28 +13,42 @@ import com.change_vision.astah.quick.internal.command.Candidates;
 import com.change_vision.astah.quick.internal.ui.candidates.CandidatesListWindow;
 
 final class CommitCommandAction extends AbstractAction {
-	private static final long serialVersionUID = 1L;
-	private final CandidatesField field;
-	private final CandidatesListWindow commandList;
-	private static final String RIGHT = "RIGHT";
+    private static final long serialVersionUID = 1L;
+    private final CandidatesField field;
+    private final CandidatesListWindow commandList;
+    private static final String RIGHT = "RIGHT";
 
-	CommitCommandAction(CandidatesField field, CandidatesListWindow commandList) {
-		super("commit-command");
-		this.field = field;
-		this.commandList = commandList;
-		InputMap inputMap = field.getInputMap();
-		ActionMap actionMap = field.getActionMap();
-		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT,KeyEvent.CTRL_DOWN_MASK), RIGHT);
-		actionMap.put(RIGHT, this);
-	}
+    CommitCommandAction(CandidatesField field, CandidatesListWindow commandList) {
+        super("commit-command");
+        this.field = field;
+        this.commandList = commandList;
+        InputMap inputMap = field.getInputMap();
+        ActionMap actionMap = field.getActionMap();
+        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, KeyEvent.CTRL_DOWN_MASK), RIGHT);
+        actionMap.put(RIGHT, this);
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		Candidates commands = commandList.getCandidates();
-		if(commands.isCommitted()) return;
-		Command current = commands.currentCommand();
-		field.setText(current.getName());
-		commandList.setCandidateText(current.getName());
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Candidates commands = commandList.getCandidates();
+        Command current = commands.currentCommand();
+        String commandName = current.getName();
+        if (commands.isCommitted()) {
+            setCommandNameToFieldOrMoveEndPosition(commandName);
+            return;
+        }
+        field.setText(commandName);
+        commandList.setCandidateText(commandName);
+    }
+
+    protected void setCommandNameToFieldOrMoveEndPosition(String commandName) {
+        String text = this.field.getText();
+        int position = text.length();
+        if (text.startsWith(commandName)) {
+            this.field.setCaretPosition(position);
+        } else {
+            this.field.setText(commandName);
+        }
+    }
 
 }
--- src/main/java/com/change_vision/astah/quick/internal/ui/candidatesfield/ExecuteCommandAction.java
+++ src/main/java/com/change_vision/astah/quick/internal/ui/candidatesfield/ExecuteCommandAction.java
@@ -19,70 +19,71 @@ import com.change_vision.astah.quick.internal.ui.QuickWindow;
 import com.change_vision.astah.quick.internal.ui.candidates.CandidatesListWindow;
 
 final class ExecuteCommandAction extends AbstractAction {
-	/**
-	 * Logger for this class
-	 */
-	private static final Logger logger = LoggerFactory.getLogger(ExecuteCommandAction.class);
+    /**
+     * Logger for this class
+     */
+    private static final Logger logger = LoggerFactory.getLogger(ExecuteCommandAction.class);
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static final String SEPARATE_COMMAND_CHAR = " ";
-	private static final String KEY = "ENTER";
-	private final CandidatesField field;
+    private static final String SEPARATE_COMMAND_CHAR = " ";
+    private static final String KEY = "ENTER";
+    private final CandidatesField field;
 
-	private final QuickWindow quickWindow;
+    private final QuickWindow quickWindow;
 
-	private final CandidatesListWindow candidatesList;
+    private final CandidatesListWindow candidatesList;
 
-	ExecuteCommandAction(CandidatesField field,QuickWindow quickWindow,CandidatesListWindow candidatesList) {
-		super("execute-command");
-		this.field = field;
-		InputMap inputMap = field.getInputMap();
-		ActionMap actionMap = field.getActionMap();
-		inputMap.put(KeyStroke.getKeyStroke(KEY), KEY);
-		actionMap.put(KEY, this);
-		this.quickWindow = quickWindow;
-		this.candidatesList = candidatesList;
-	}
+    ExecuteCommandAction(CandidatesField field, QuickWindow quickWindow,
+            CandidatesListWindow candidatesList) {
+        super("execute-command");
+        this.field = field;
+        InputMap inputMap = field.getInputMap();
+        ActionMap actionMap = field.getActionMap();
+        inputMap.put(KeyStroke.getKeyStroke(KEY), KEY);
+        actionMap.put(KEY, this);
+        this.quickWindow = quickWindow;
+        this.candidatesList = candidatesList;
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		logger.trace("execute");
-		Candidates candidates = candidatesList.getCandidates();
-		Command command = candidates.currentCommand();
-		String commandName = command.getName();
-		String fieldText = field.getText();
-		Candidate candidate = candidates.current();
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        logger.trace("execute");
+        Candidates candidates = candidatesList.getCandidates();
+        Command command = candidates.currentCommand();
+        String commandName = command.getName();
+        String fieldText = field.getText();
+        Candidate candidate = candidates.current();
 
-		quickWindow.close();
-		if(command instanceof CandidatesProvider){
-			((CandidatesProvider)command).execute(candidate);
-			return;
-		}
-		if (fieldText.startsWith(commandName) != false
-				&& fieldText.length() == commandName.length()) {
-			try {
-				command.execute();
-			} catch (Exception ex) {
-				quickWindow.notifyError("Alert", ex.getMessage());
-			}
-			return;
-		}
-		String[] splitedCommand = fieldText.split(SEPARATE_COMMAND_CHAR);
-		String[] args = null;
-		int commandRange = commandName.split(SEPARATE_COMMAND_CHAR).length;
-		if (splitedCommand.length > commandRange) {
-			args = Arrays.copyOfRange(splitedCommand, commandRange,
-					splitedCommand.length);
-		}
-		logger.trace("commandList:execute commandName:'{}',args:'{}'",
-				commandName, args);
-		try {
-			command.execute(args);
-		} catch (Exception ex) {
-			quickWindow.notifyError("Alert", ex.getMessage());
-		}
-	}
+        quickWindow.close();
+        if (command instanceof CandidatesProvider) {
+            if (command.getClass().isInstance(candidate)) {
+                candidate = null;
+            }
+            ((CandidatesProvider) command).execute(candidate);
+            return;
+        }
+        if (fieldText.startsWith(commandName) != false
+                && fieldText.length() == commandName.length()) {
+            try {
+                command.execute();
+            } catch (Exception ex) {
+                quickWindow.notifyError("Alert", ex.getMessage());
+            }
+            return;
+        }
+        String[] splitedCommand = fieldText.split(SEPARATE_COMMAND_CHAR);
+        String[] args = null;
+        int commandRange = commandName.split(SEPARATE_COMMAND_CHAR).length;
+        if (splitedCommand.length > commandRange) {
+            args = Arrays.copyOfRange(splitedCommand, commandRange, splitedCommand.length);
+        }
+        logger.trace("commandList:execute commandName:'{}',args:'{}'", commandName, args);
+        try {
+            command.execute(args);
+        } catch (Exception ex) {
+            quickWindow.notifyError("Alert", ex.getMessage());
+        }
+    }
 
 }
-
