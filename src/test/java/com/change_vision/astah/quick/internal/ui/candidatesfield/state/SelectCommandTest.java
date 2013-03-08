9693ef9e4229dc989f45324f84c343d47916929c
Hiroki Kondo <kompiro@gmail.com>
2013-03-08 16:15:32 +0900

    show only enabled command when key is null or empty

--- src/test/java/com/change_vision/astah/quick/internal/ui/candidatesfield/state/SelectCommandTest.java
+++ src/test/java/com/change_vision/astah/quick/internal/ui/candidatesfield/state/SelectCommandTest.java
@@ -19,122 +19,132 @@ import com.change_vision.astah.quick.internal.command.model.SelectModelCommandFa
 
 public class SelectCommandTest {
 
-	private SelectCommand state;
-	
-	@Mock
-	private SelectModelCommandFactory commandFactory;
+    private SelectCommand state;
 
-	@Mock
-	private Command closeProjectCommand;
+    @Mock
+    private SelectModelCommandFactory commandFactory;
 
-	@Mock
-	private Command createClassCommand;
+    @Mock
+    private Command closeProjectCommand;
 
-	@Mock
-	private Command createPackageCommand;	
-	
-	@Mock
-	private Command notEnabledCommand;
-	
-	@Mock
-	private Command foundClassModelCommand;
+    @Mock
+    private Command createClassCommand;
 
-	@Mock
-	private Command foundPackageModelCommand;
-	
-	private List<Command> allCommands;
+    @Mock
+    private Command createPackageCommand;
 
-	@Before
-	public void before() throws Exception {
-		MockitoAnnotations.initMocks(this);
-		
-		state = new SelectCommand();
-		state.clear();
-		state.setCommandFactory(commandFactory);
-		
-		createMockCommand(closeProjectCommand, "close project", true);
-		createMockCommand(createClassCommand, "create class", true);
-		createMockCommand(createPackageCommand, "create package", true);
-		createMockCommand(notEnabledCommand, "not enabled", false);
-		createMockCommand(foundClassModelCommand, "Class", true);
-		createMockCommand(foundPackageModelCommand, "Class Package", true);
-		
-		state.add(closeProjectCommand);
-		state.add(createClassCommand);
-		state.add(createPackageCommand);
-		state.add(notEnabledCommand);
-		
-		allCommands = new ArrayList<Command>();
+    @Mock
+    private Command notEnabledCommand;
 
-		allCommands.add(closeProjectCommand);
-		allCommands.add(createClassCommand);
-		allCommands.add(createPackageCommand);
-		allCommands.add(notEnabledCommand);
-	}
-	
-	@After
+    @Mock
+    private Command foundClassModelCommand;
+
+    @Mock
+    private Command foundPackageModelCommand;
+
+    private List<Command> allCommands;
+
+    @Before
+    public void before() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        state = new SelectCommand();
+        state.clear();
+        state.setCommandFactory(commandFactory);
+
+        createMockCommand(closeProjectCommand, "close project", true);
+        createMockCommand(createClassCommand, "create class", true);
+        createMockCommand(createPackageCommand, "create package", true);
+        createMockCommand(notEnabledCommand, "not enabled", false);
+        createMockCommand(foundClassModelCommand, "Class", true);
+        createMockCommand(foundPackageModelCommand, "Class Package", true);
+
+        state.add(closeProjectCommand);
+        state.add(createClassCommand);
+        state.add(createPackageCommand);
+        state.add(notEnabledCommand);
+
+        allCommands = new ArrayList<Command>();
+
+        allCommands.add(closeProjectCommand);
+        allCommands.add(createClassCommand);
+        allCommands.add(createPackageCommand);
+        allCommands.add(notEnabledCommand);
+    }
+
+    @After
     public void after() throws Exception {
         state.initCommands();
     }
 
-	private Command createMockCommand(Command target, String commandName,
-			boolean enable) {
-		when(target.getName()).thenReturn(commandName);
-		when(target.isEnabled()).thenReturn(enable);
-		return target;
-	}
+    private Command createMockCommand(Command target, String commandName, boolean enable) {
+        when(target.getName()).thenReturn(commandName);
+        when(target.isEnabled()).thenReturn(enable);
+        return target;
+    }
 
-	@Test
-	public void filterWithNull() {
-		Candidate[] candidates = state.filter(null);
-		
-		Candidate[] expected = allCommands.toArray(new Candidate[0]);
-		assertThat(candidates,is(expected));
-	}
-	
-	@Test
-	public void filterWithEmpty() throws Exception {
-		Candidate[] candidates = state.filter("");
-		
-		assertThat(candidates.length,is(4));		
-	}
-	
-	@Test
-	public void filterWithC() throws Exception {
-		Candidate[] candidates = state.filter("c");
-		
-		assertThat(candidates.length,is(3));		
-	}
+    @Test
+    public void filterWithNull() {
+        Candidate[] candidates = state.filter(null);
 
-	@Test
-	public void filterWithCreate() throws Exception {
-		Candidate[] candidates = state.filter("create");
-		
-		assertThat(candidates.length,is(2));
-	}
-	
-	@Test
-	public void filterWithSpeficiedCommand() throws Exception {
-		Candidate[] candidates = state.filter("create class");
-		
-		assertThat(candidates.length,is(1));
-		assertThat(candidates[0].getName(),is("create class"));
+        Candidate[] expected = new Candidate[]{
+                closeProjectCommand,
+                createClassCommand,
+                createPackageCommand
+        };
+        assertThat(candidates.length, is(3));
+        assertThat(candidates, is(expected));
+    }
 
-		candidates = state.filter("create package");
-		
-		assertThat(candidates.length,is(1));
-		assertThat(candidates[0].getName(),is("create package"));
-	}
-	
-	@Test
-	public void filterWithFoundModels() throws Exception {
-		ArrayList<Candidate> created = new ArrayList<Candidate>();
-		created.add(foundClassModelCommand);
-		created.add(foundPackageModelCommand);
-		when(commandFactory.create("Cla")).thenReturn(created);
-		
-		Candidate[] candidates = state.filter("Cla");
-		assertThat(candidates.length,is(2));
-		assertThat(candidates[0].getName(),is("Class"));
-	}
+    @Test
+    public void filterWithEmpty() throws Exception {
+        Candidate[] candidates = state.filter("");
+
+        Candidate[] expected = new Candidate[]{
+                closeProjectCommand,
+                createClassCommand,
+                createPackageCommand
+        };
+        assertThat(candidates.length, is(3));
+        assertThat(candidates, is(expected));
+    }
+
+    @Test
+    public void filterWithC() throws Exception {
+        Candidate[] candidates = state.filter("c");
+
+        assertThat(candidates.length, is(3));
+    }
+
+    @Test
+    public void filterWithCreate() throws Exception {
+        Candidate[] candidates = state.filter("create");
+
+        assertThat(candidates.length, is(2));
+    }
+
+    @Test
+    public void filterWithSpeficiedCommand() throws Exception {
+        Candidate[] candidates = state.filter("create class");
+
+        assertThat(candidates.length, is(1));
+        assertThat(candidates[0].getName(), is("create class"));
+
+        candidates = state.filter("create package");
+
+        assertThat(candidates.length, is(1));
+        assertThat(candidates[0].getName(), is("create package"));
+    }
+
+    @Test
+    public void filterWithFoundModels() throws Exception {
+        ArrayList<Candidate> created = new ArrayList<Candidate>();
+        created.add(foundClassModelCommand);
+        created.add(foundPackageModelCommand);
+        when(commandFactory.create("Cla")).thenReturn(created);
+
+        Candidate[] candidates = state.filter("Cla");
+        assertThat(candidates.length, is(2));
+        assertThat(candidates[0].getName(), is("Class"));
+    }
 }
