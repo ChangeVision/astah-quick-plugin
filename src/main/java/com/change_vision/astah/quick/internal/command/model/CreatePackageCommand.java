6a8c9ceefb8cca9ed46a42471f0669abd0ab4416
Hiroki Kondo <kompiro@gmail.com>
2013-03-12 13:31:34 +0900

    fix to notify error when the name of element is already existed.

--- src/main/java/com/change_vision/astah/quick/internal/command/model/CreatePackageCommand.java
+++ src/main/java/com/change_vision/astah/quick/internal/command/model/CreatePackageCommand.java
@@ -1,11 +1,15 @@
 package com.change_vision.astah.quick.internal.command.model;
 
+import static java.lang.String.format;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.change_vision.astah.quick.command.CandidateIconDescription;
 import com.change_vision.astah.quick.command.Command;
+import com.change_vision.astah.quick.command.exception.ExecuteCommandException;
 import com.change_vision.astah.quick.internal.command.AstahCommandIconDescription;
+import com.change_vision.jude.api.inf.model.INamedElement;
 import com.change_vision.jude.api.inf.view.IconDescription;
 
 public class CreatePackageCommand implements Command{
@@ -20,10 +24,15 @@ public class CreatePackageCommand implements Command{
 	}
 
 	@Override
-	public void execute(String... args) {
+	public void execute(String... args) throws ExecuteCommandException {
 		if(args == null || args.length == 0) throw new IllegalArgumentException("'create package' command needs argument.");
 		for (String packageName : args) {
 			logger.trace("create package '{}'",packageName);
+            INamedElement[] found = api.find(packageName);
+            if (found.length > 0) {
+                String message = format("'%s' is already existed.",packageName);
+                throw new ExecuteCommandException(message);
+            }
 			api.createPackage(packageName);
 		}
 	}
