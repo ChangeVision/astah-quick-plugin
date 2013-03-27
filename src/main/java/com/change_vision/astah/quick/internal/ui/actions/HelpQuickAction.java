f0a5a39fe03a8f8ab4d2779a554134ab46b833ed
Hiroki Kondo <kompiro@gmail.com>
2013-03-27 17:54:57 +0900

    add quick command help window menu

--- src/main/java/com/change_vision/astah/quick/internal/ui/actions/HelpQuickAction.java
+++ src/main/java/com/change_vision/astah/quick/internal/ui/actions/HelpQuickAction.java
@@ -1,31 +1,29 @@
 package com.change_vision.astah.quick.internal.ui.actions;
 
-import java.awt.Window;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import com.change_vision.astah.quick.internal.Activator;
 import com.change_vision.astah.quick.internal.ui.QuickInterfaceUI;
-import com.change_vision.astah.quick.internal.ui.configure.ConfigWindow;
+import com.change_vision.astah.quick.internal.ui.installed.InstalledWizardDialog;
 import com.change_vision.jude.api.inf.ui.IPluginActionDelegate;
 import com.change_vision.jude.api.inf.ui.IWindow;
 
-public class ConfigureQuickAction implements IPluginActionDelegate {
+public class HelpQuickAction implements IPluginActionDelegate {
 
     @Override
     public Object run(IWindow arg0) throws UnExpectedException {
         Activator instance = Activator.getInstance();
         final QuickInterfaceUI ui = instance.getUI();
         ui.uninstall();
-        Window frame = arg0.getParent();
-        ConfigWindow window = new ConfigWindow(frame);
+        InstalledWizardDialog window = new InstalledWizardDialog(arg0.getParent());
         window.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosed(WindowEvent e) {
                 ui.install();
             }
         });
-        window.open();
+        window.setVisible(true);
         return null;
     }
 
--- src/main/java/com/change_vision/astah/quick/internal/ui/installed/InstalledWizardDialog.java
+++ src/main/java/com/change_vision/astah/quick/internal/ui/installed/InstalledWizardDialog.java
@@ -3,17 +3,17 @@ package com.change_vision.astah.quick.internal.ui.installed;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Window;
 
 import javax.swing.JDialog;
-import javax.swing.JFrame;
 
 import com.change_vision.astah.quick.internal.Messages;
 
 @SuppressWarnings("serial")
 public class InstalledWizardDialog extends JDialog {
 
-    public InstalledWizardDialog(JFrame parent){
-        super(parent);
+    public InstalledWizardDialog(Window window){
+        super(window);
         setModalityType(ModalityType.APPLICATION_MODAL);
         InstalledWizardPanel panel = new InstalledWizardPanel(this);
         getContentPane().add(panel);
@@ -21,7 +21,7 @@ public class InstalledWizardDialog extends JDialog {
 
         // for dialog unique(sometimes license dialog is opened.)
         setModal(true);
-        Rectangle parentBounds = parent.getBounds();
+        Rectangle parentBounds = window.getBounds();
         Point centerPoint = new Point();
         centerPoint.setLocation(parentBounds.getCenterX(), parentBounds.getCenterY());
         Dimension size = getSize();
